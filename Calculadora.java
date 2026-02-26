e6163ec8de0e7593f85f4a37afa37c38ee35a3d7
enzoHCC <enzohccarvalho@gmail.com>
2026-02-26 19:33:32 -0300

    Update and rename CalculadoraCopy.java to Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class CalculadoraCopy {
+public class Calculadora {
   
   private static String respostaUsuario;
   private static String respostaUsuario2;
@@ -8,8 +8,8 @@ public class CalculadoraCopy {
     
     Scanner inputScanner = new Scanner(System.in);
     boolean sair=false;
-    
-    while(sair==false){
+    boolean respostaErrada =true;
+    while(sair==false | respostaErrada==true){
       System.out.println("Digite a operação que você deseja entre as opções abaixo;");
       System.out.println("[S]-Soma");
       System.out.println("[L]-Subtração");
@@ -18,6 +18,7 @@ public class CalculadoraCopy {
       respostaUsuario = inputScanner.nextLine().trim();
     
       if(respostaUsuario.equals("S") || respostaUsuario.equals("s")) {
+      respostaErrada=true;
       
       System.out.println("Digite o primeiro número a ser somado e tecle Enter: ");
       respostaUsuario = inputScanner.nextLine().trim();
@@ -31,7 +32,8 @@ public class CalculadoraCopy {
       System.out.println("A soma dos dois numeros são: " + (resultado));
 
       } else if(respostaUsuario.equals("L") || respostaUsuario.equals("l")){
-        
+        respostaErrada=true;
+
         System.out.println("Digite o primeiro número a ser subtraído e tecle Enter: ");
         respostaUsuario = inputScanner.nextLine().trim();
         double numeroUm = Double.valueOf(respostaUsuario);
@@ -44,7 +46,8 @@ public class CalculadoraCopy {
         System.out.println("A subtração desses dois números são: " + (resultado));
 
       } else if(respostaUsuario.equals("D") || respostaUsuario.equals("d")){
-        
+        respostaErrada=true;
+
         System.out.println("Digite o primeiro número a ser dividido: ");
         respostaUsuario = inputScanner.nextLine().trim();
         double numeroUm = Double.valueOf(respostaUsuario);
@@ -57,7 +60,8 @@ public class CalculadoraCopy {
         System.out.println("A divisão desses números são: " + (resultado));
 
       } else if(respostaUsuario.equals("M") || respostaUsuario.equals("m")) {
-        
+        respostaErrada=true;
+
         System.out.println("Digite o primeiro número a ser multiplicado: ");
         respostaUsuario = inputScanner.nextLine().trim();
         double numeroUm = Double.valueOf(respostaUsuario);
@@ -69,8 +73,15 @@ public class CalculadoraCopy {
         double resultado = (double) numeroUm *numeroDois;
         System.out.println("A multiplicação desses números são:" + (resultado ));
 
-      } 
-    
+      }else {
+        System.out.println("Resposta Inválida! Por favor tente novamente: ");
+        try{
+          Thread.sleep(5000);
+        } catch(Exception ev){
+          System.err.println("Error: " + ev);
+        }
+      }
+
       System.out.println("Deseja Continuar:");
       System.out.println("[1]-Sim"); 
       System.out.println("[2]-Não");
@@ -80,13 +91,11 @@ public class CalculadoraCopy {
 
       if (rFinal == 1) {
         sair = false;
-      
-      } else {
-        sair = true;
-      }
+      } 
     } 
     if (sair == true){
       inputScanner.close();
     }
   }
 }
+
